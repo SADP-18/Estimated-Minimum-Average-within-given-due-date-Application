d8dcbe428fa75dc39130b4b5c5bc8651c1d72da7
AssassinX-01 <139675775+AssassinX-01@users.noreply.github.com>
2024-02-26 17:33:19 +0700

    Add files via upload

---
+++ AppLauncher.java
@@ -0,0 +1,32 @@
+import db_objs.User;
+import gui.GoalmenuGUI;
+import gui.LoginGUI;
+import gui.SettingsGUI;
+import gui.signUpGUI;
+import gui.stockMenuGUI;
+import gui.ContactUsGUI;
+
+
+import javax.swing.*;
+import java.math.BigDecimal;
+
+public class AppLauncher {
+    public static void main(String[] args) {
+        //use invokeLater to make updates to the GUI more thread-safe
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+             new LoginGUI().setVisible(true);
+            // new signUpGUI().setVisible(true);
+               // new GoalmenuGUI(
+                   // new User(1, "username", "password", new BigDecimal("20.00"))
+               // ).setVisible(true);
+              // new stockMenuGUI(new User(1, "username", "password", new BigDecimal("20.00"))
+              // ).setVisible(true);
+              //new ContactUsGUI().setVisible(true);
+              //new SettingsGUI(null).setVisible(true);
+               
+            }
+        });
+    }
+}
