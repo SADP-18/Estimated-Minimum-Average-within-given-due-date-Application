6ef20afbb5ca4e27a163a4b442cd6964d2b58608
AssassinX-01 <139675775+AssassinX-01@users.noreply.github.com>
2024-03-02 10:43:08 +0700

    Add files via upload

---
+++ LoginSystem.java
@@ -0,0 +1,7 @@
+import gui.LoginGUI;
+
+public class LoginSystem {   
+    public static void main(String args[]) {               
+    new LoginGUI().setVisible(true);
+    }
+}
---
+++ signUp.java
@@ -0,0 +1,8 @@
+import gui.signUpGUI;
+
+public class signUp {
+    public static void main(String[] args) {              
+    new signUpGUI().setVisible(true);
+    }
+}
+
